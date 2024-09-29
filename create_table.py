b5def51e4d2fb25b9b0ace6af4d90bdcd520085c
marinasemak <marina.semak@gmail.com>
2024-09-29 21:32:52 +0300

    create db and fill data

---
+++ create_table.py
@@ -0,0 +1,67 @@
+import logging
+
+from psycopg2 import DatabaseError
+
+from connection import create_connection
+
+
+def create_table(conn, sql_expression):
+    c = conn.cursor()
+    try:
+        c.execute(sql_expression)
+        conn.commit()
+    except DatabaseError as err:
+        logging.error(err)
+        conn.rollback()
+    finally:
+        c.close()
+
+
+if __name__ == '__main__':
+    sql_create_students = """CREATE TABLE IF NOT EXISTS students (
+     id SERIAL PRIMARY KEY,
+     name VARCHAR(120) NOT NULL,
+     group_id INT,
+     FOREIGN KEY (group_id) REFERENCES groups (id) ON DELETE SET NULL ON UPDATE CASCADE
+    );"""
+
+    sql_create_group = """CREATE TABLE IF NOT EXISTS groups (
+         id SERIAL PRIMARY KEY,
+         name VARCHAR(120) NOT NULL
+        );"""
+
+    sql_create_lecturer = """CREATE TABLE IF NOT EXISTS lecturers (
+         id SERIAL PRIMARY KEY,
+         name VARCHAR(120) NOT NULL
+        );"""
+
+    sql_create_subjects = """CREATE TABLE IF NOT EXISTS subjects (
+         id SERIAL PRIMARY KEY,
+         name VARCHAR(120) NOT NULL,
+         lecturer_id INT,
+         FOREIGN KEY (lecturer_id) REFERENCES lecturers (id) ON DELETE SET NULL ON UPDATE CASCADE
+        );"""
+
+    sql_create_students_grades = """CREATE TABLE IF NOT EXISTS students_grades (
+         id SERIAL PRIMARY KEY,
+         grade_value INT,
+         student_id INT NOT NULL,
+         subject_id INT NOT NULL,
+         FOREIGN KEY (student_id) REFERENCES students (id) ON DELETE CASCADE ON UPDATE CASCADE,
+         FOREIGN KEY (subject_id) REFERENCES subjects (id) ON DELETE CASCADE ON UPDATE CASCADE,
+         date DATE NOT NULL
+        );"""
+
+    try:
+        with create_connection() as conn:
+            if conn is not None:
+                # create_table(conn, sql_create_students)
+                # create_table(conn, sql_create_group)
+                # create_table(conn, sql_create_lecturer)
+                # create_table(conn, sql_create_subjects)
+                create_table(conn, sql_create_students_grades)
+                logging.info("the table is created")
+            else:
+                logging.error('Error: can\'t create the database connection')
+    except RuntimeError as err:
+        logging.error(err)
---
+++ insert_data.py
@@ -0,0 +1,105 @@
+import logging
+import random
+
+from faker import Faker
+
+from datetime import date
+from random import randint
+from psycopg2 import DatabaseError
+
+from connection import create_connection
+
+fake = Faker()
+STUDENTS_COUNT = 50
+GROUPS_COUNT = 3
+LECTURER_COUNT = 5
+SUBJECTS_COUNT = 8
+fake_data = Faker()
+
+
+def generate_fake_data(number_students, number_lecturers) -> tuple():
+    fake_students = []
+    fake_groups = ["GroupA", "GroupB", "GroupC"]
+    fake_lecturers = []
+    fake_subjects = ["Geology", "Geography", "Chemistry", "Geophysics", "Mathematics"]
+
+    for _ in range(number_students):
+        fake_students.append(fake_data.name())
+
+    for _ in range(number_lecturers):
+        fake_lecturers.append(fake_data.name())
+
+    return fake_groups, fake_students, fake_lecturers, fake_subjects
+
+
+def prepare_data(groups, students, lecturers, subjects) -> tuple():
+    for_students = []
+
+    for student in students:
+        for_students.append((student, randint(1, GROUPS_COUNT)))
+
+    for_groups = []
+
+    for group in groups:
+        for_groups.append((group,))
+
+    for_lecturers = []
+
+    for lect in lecturers:
+        for_lecturers.append((lect,))
+
+    for_subjects = []
+
+    for subj in subjects:
+        for_subjects.append((subj, randint(1, LECTURER_COUNT)))
+
+    for_grades = []
+    start_date = date(2023, 9, 1)
+    end_date = date(2024, 6, 30)
+
+    for student in range(1, STUDENTS_COUNT + 1):
+        for _ in range(1, 20):
+            for_grades.append((randint(60, 100), student, randint(1, SUBJECTS_COUNT),
+                               fake_data.date_between(start_date, end_date)))
+
+    return for_groups, for_students, for_lecturers, for_subjects, for_grades
+
+
+def insert_data(conn, groups, students, lecturers, subjects, grades):
+    cur = conn.cursor()
+    try:
+        sql_to_groups = "INSERT INTO groups(name) VALUES(%s)"
+        cur.executemany(sql_to_groups, groups)
+
+        sql_to_students = "INSERT INTO students(name, group_id) VALUES(%s, %s)"
+        cur.executemany(sql_to_students, students)
+
+        sql_to_lecturers = "INSERT INTO lecturers(name) VALUES(%s)"
+        cur.executemany(sql_to_lecturers, lecturers)
+
+        sql_to_subjects = "INSERT INTO subjects(name, lecturer_id) VALUES(%s, %s)"
+        cur.executemany(sql_to_subjects, subjects)
+
+        sql_to_grades = "INSERT INTO students_grades(grade_value, student_id, subject_id, date) VALUES(%s, %s, %s, %s)"
+        cur.executemany(sql_to_grades, grades)
+
+        conn.commit()
+    except DatabaseError as err:
+        logging.error(err)
+        conn.rollback()
+    finally:
+        cur.close()
+
+
+if __name__ == '__main__':
+    groups, students, lecturers, subjects, grades = prepare_data(*generate_fake_data(STUDENTS_COUNT, LECTURER_COUNT))
+    try:
+        with create_connection() as conn:
+            if conn is not None:
+                insert_data(conn, groups, students, lecturers, subjects, grades)
+            else:
+                print('Error: can\'t create the database connection')
+    except RuntimeError as err:
+        logging.error(err)
+    # print(f"{groups}\n {students}\n {lecturers}\n {subjects}\n {grades}")
+    # print(f"{groups}")
